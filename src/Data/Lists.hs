fc80f3f3f7f5e416763010002caa7cdc1cc9c810
nikivazou <nikivazou@gmail.com>
2019-06-20 12:00:21 +0200

    prettify mapFusion

--- src/Data/Lists.hs
+++ src/Data/Lists.hs
@@ -5,11 +5,12 @@ import Prelude hiding (length)
 {-@ infix   :   @-}
 
 {-@ measure length @-}
+{-@ length :: [a] -> Nat @-} 
 length :: [a] -> Int 
 length [] = 0 
 length (x:xs) = 1 + length xs     
 
 {-@ reflect cons @-}
-{-@ cons :: x:a -> xs:[a] -> {z:[a] | z == x:xs} @-}
+{-@ cons :: x:a -> xs:[a] -> {z:[a] | z == x:xs && length z == length xs + 1} @-}
 cons :: a -> [a] -> [a]
 cons x xs = x:xs
--- src/examples/MapFusion.hs
+++ src/examples/MapFusion.hs
@@ -1,4 +1,5 @@
 {-@ LIQUID "--reflection" @-}
+{-  LIQUID "--ple"        @-}
 module MapFusion where 
 
 import Prelude hiding (mapM, Applicative (..), Monad (..), length)    
@@ -14,11 +15,12 @@ import Proof.Quantified
 
 {-@ reflect mapM @-}
 mapM :: (a -> Tick b) -> [a] -> Tick [b]
+{-@ mapM :: (a -> Tick b) -> xs:[a] -> Tick {os:[b] | length os == length xs} @-}
 mapM f []     = pure [] 
 mapM f (x:xs) = step 1 (liftA2 cons (f x) (mapM f xs))
 
 
-mapFusion :: Eq c => (a -> Tick b) -> (b -> Tick c) -> [a] -> () 
+mapFusion :: (a -> Tick b) -> (b -> Tick c) -> [a] -> () 
 {-@ mapFusion :: f:(a -> Tick b) -> g:(b -> Tick c) -> xs:[a] 
               -> { (tval (mapM f xs >>= mapM g)  == tval (mapM (f >=> g) xs)) && 
                    (tcost (mapM f xs >>= mapM g) == length xs  + tcost (mapM (f >=> g) xs))
@@ -32,25 +34,28 @@ mapFusion f g []
   *** QED 
 mapFusion f g (x:xs) 
   =   mapM f (x:xs) >>= mapM g
-  <=> step 1 (liftA2 cons (f x) (mapM f xs)) >>= mapM g
-  >== 1 ==> 
+  <=> (step 1 (liftA2 cons (f x) (mapM f xs))) >>= mapM g
+  >== 1 ==>   liftA2 cons (f x) (mapM f xs) >>= mapM g
+  <=> 
       Tick (cf+cfs) (fx `cons` fxs) >>= mapM g
-  <=> Tick (cf+cfs+cgs) gfxs
-      ? (mapM g (fx:fxs) ==. step 1 (liftA2 cons (g fx) (mapM g fxs)) *** QED)
-  <=> Tick (1+cfg+cfgs) (fgx `cons` fgxs)   
-      ? mapFusion f g xs 
+  <=> (let Tick cgs (gfx:gfxs)  = mapM g (fx:fxs) in 
+        Tick (cf+cfs+cgs) (gfx:gfxs))
+
+  <=> (let Tick cgs (gfx:gfxs)  = step 1 (liftA2 cons (g fx) (mapM g fxs)) in 
+      Tick (cf+cfs+cgs) (gfx:gfxs))
+
+  <=> step (1+cf+cfs) (liftA2 cons (g fx) (mapM g fxs))
+  <=> step (1+cf) (liftA2 cons (g fx) (mapM f xs >>= mapM g))
+      ? mapFusion f g xs
   >== length xs ==>  
-      Tick (1+cfg + cfgsr) (fgx:fgxsr)
+      step (1+cf) (liftA2 cons (g fx)   (mapM (f >=> g) xs))
   <=> step 1 (liftA2 cons ((f >=> g) x) (mapM (f >=> g) xs))      
   <=> mapM (f >=> g) (x:xs) 
   *** QED 
   where
     Tick cf fx     = f x 
     Tick cfs fxs   = mapM f xs   
-    Tick cgs gfxs  = mapM g (fx:fxs)  
-    Tick cfg fgx   = (f >=> g) x
     Tick cfgs fgxs = mapM f xs >>= mapM g
-    Tick cfgsr fgxsr = mapM (f >=> g) xs
 
 
 
